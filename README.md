d2a1c6e9f5860a22235662472871eb43d07b6f30
Gabriele Iannetti <g.iannetti@gsi.de>
2021-10-01 09:00:09 +0200

    Refactor arguments for exporter execution.

--- README.md
+++ README.md
@@ -20,11 +20,7 @@ go build -o prometheus-cluster-exporter *.go
 
 ### Lustre Exporter
 
-A [Lustre exporter](https://github.com/GSI-HPC/lustre_exporter) that exposes the following metrics to Prometheus:
-
-* lustre\_job\_stats\_total
-* lustre\_job\_read\_bytes\_total
-* lustre\_job\_write\_bytes\_total
+[Lustre exporter](https://github.com/GSI-HPC/lustre_exporter) that exposes enabled Lustre Jobstats on the filesystem.
 
 ### Squeue Command
 
@@ -41,28 +37,14 @@ The getent command is required for the uid to user and group mapping used for th
 
 ### Parameter
 
-| Name      | Default           | Description                                                                                                                        |
-| --------- | ----------------- | ---------------------------------------------------------------------------------------------------------------------------------- |
-| log       | INFO              | Logging level                                                                                                                      | 
-| port      | 9846              | The port to listen on for HTTP requests                                                                                            |
-| timeout   | 15                | HTTP request timeout in seconds for exporting Lustre Jobstats on Prometheus HTTP API                                               |
-| urlReads  | Site specific URL | Query URL to the Prometheus HTTP API that exports the Lustre jobstats read throughput rate                                         |
-| urlWrites | Site specific URL | Query URL to the Prometheus HTTP API that exports the Lustre jobstats write throughput rate                                        |
-
-### Exporting Lustre Jobstats Throughput Rate
-
-The Lustre jobstats throughput rates are calculated on the Prometheus server and exported via HTTP API.  
-
-A HTTP query for setting the urlReads and urlWrites parameter consists of:
-
-* Server endpoint: `http://prom-server:9090/`
-* HTTP API endpoint: `api/v1/query`
-* Query parameter: `?query=`
-* Query string
-    * Reads: `sum%20by%28jobid%29%28irate%28lustre_job_write_bytes_total[1m]%29!=0%29`
-    * Writes: `sum%20by%28jobid%29%28irate%28lustre_job_write_bytes_total[1m]%29!=0%29`
-
-> Some special character are defined in UTF-8 hexadecimal in the query strings.
+| Name       | Default           | Description                                                                                                                        |
+| ---------- | ----------------- | ---------------------------------------------------------------------------------------------------------------------------------- |
+| version    | false             | Print version                                                                                                                      | 
+| promserver |                   | [REQUIRED] Prometheus Server to be used e.g. http://prometheus-server:9090                                                         |
+| log        | INFO              | Sets log level - INFO, DEBUG or TRACE                                                                                              | 
+| port       | 9846              | The port to listen on for HTTP requests                                                                                            |
+| timeout    | 15                | HTTP request timeout in seconds for exporting Lustre Jobstats on Prometheus HTTP API                                               |
+| timerange  | 1m                | Time range used for rate function on the retrieving Lustre metrics from Prometheus - A three digit number with unit s, m, h or d   |
 
 ### Running in a Productive Environment
 
@@ -71,10 +53,9 @@ since the target host should be most stable.
 
 ### Prometheus Scrape Settings
 
-Depending on the required resolution and runtimes of an exporter  
-the `scrape interval` should be set as appropriate e.g. at least 1m or higher.  
-
-The `scrape timeout` should also be set, but close to the specific scrape interval.
+Depending on the required resolution and runtime of the exporter,  
+* the `scrape interval` should be set as appropriate e.g. at least 1 minute or higher.  
+* the `scrape timeout` should be set close to the specified scrape interval.
 
 ## Metrics
 
--- main.go
+++ main.go
@@ -19,6 +19,9 @@ import (
 	"flag"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -27,16 +30,25 @@ import (
 )
 
 const (
-	version                            = "1.1.3"
-	namespace                          = "cluster"
-	namespaceInternals                 = "cluster_exporter"
-	defaultPort                        = "9846"
-	defaultRequestTimeout              = 15
-	defaultLogLevel                    = "INFO" // [INFO || DEBUG || TRACE]
-	defaulturlLustreMetadataOperations = "http://prom-server:9090/api/v1/query?query=round%28sum%20by%28jobid%29%28irate%28lustre_job_stats_total[1m]%29%3E=1%29%29"
-	defaultURLLustreJobReadBytes       = "http://prom-server:9090/api/v1/query?query=sum%20by%28jobid%29%28irate%28lustre_job_read_bytes_total[1m]%29!=0%29"
-	defaultURLLustreJobWriteBytes      = "http://prom-server:9090/api/v1/query?query=sum%20by%28jobid%29%28irate%28lustre_job_write_bytes_total[1m]%29!=0%29"
+	version                 = "1.1.3"
+	namespace               = "cluster"
+	namespaceInternals      = "cluster_exporter"
+	httpApi                 = "/api/v1/query"
+	queryParameter          = "?query="
+	queryMetadataOperations = "round%28sum%20by%28jobid%29%28irate%28lustre_job_stats_total[__TIME_RANGE__]%29%3E=1%29%29"
+	queryJobReadBytes       = "sum%20by%28jobid%29%28irate%28lustre_job_read_bytes_total[__TIME_RANGE__]%29!=0%29"
+	queryJobWriteBytes      = "sum%20by%28jobid%29%28irate%28lustre_job_write_bytes_total[__TIME_RANGE__]%29!=0%29"
+	defaultLogLevel         = "INFO"
+	defaultPort             = "9846"
+	defaultRequestTimeout   = 15
+	defaultTimeRange        = "1m"
 )
+
+type urlExportLustreMetrics struct {
+	metadataOperations string
+	jobReadBytes       string
+	jobWriteBytes      string
+}
 
 func initLogging(logLevel string) {
 
@@ -47,37 +59,79 @@ func initLogging(logLevel string) {
 	} else if logLevel == "TRACE" {
 		log.SetLevel(log.TraceLevel)
 	} else {
-		log.Panicln("Not supported log level set!")
+		log.Panicln("Not supported log level set")
 	}
 
 	log.SetOutput(os.Stdout)
 }
 
+func validateTimeRange(timeRange string) {
+
+	lenTimeRange := len(timeRange)
+
+	if lenTimeRange < 2 || lenTimeRange > 4 {
+		log.Panic("Time range length is not supported: ", timeRange)
+	}
+
+	reTimeRangeUnit := regexp.MustCompile("s|m|h|d")
+
+	timeRangeUnit := timeRange[lenTimeRange-1:]
+	timeRangeNumber := timeRange[:lenTimeRange-1]
+
+	if reTimeRangeUnit.MatchString(timeRangeUnit) == false {
+		log.Panic("Time range unit is not supported: ", timeRangeUnit)
+	}
+
+	_, err := strconv.Atoi(timeRangeNumber)
+
+	if err != nil {
+		log.Panic("Time range number could not be coverted to an integer: ", timeRangeNumber)
+	}
+}
+
+func newUrlExportLustreMetrics(server string, timeRange string) *urlExportLustreMetrics {
+
+	validateTimeRange(timeRange)
+
+	serverQueryEndpoint := server + httpApi + queryParameter
+
+	return &urlExportLustreMetrics{
+		metadataOperations: serverQueryEndpoint + strings.Replace(queryMetadataOperations, "__TIME_RANGE__", timeRange, 1),
+		jobReadBytes:       serverQueryEndpoint + strings.Replace(queryJobReadBytes, "__TIME_RANGE__", timeRange, 1),
+		jobWriteBytes:      serverQueryEndpoint + strings.Replace(queryJobWriteBytes, "__TIME_RANGE__", timeRange, 1),
+	}
+}
+
 func main() {
 
 	printVersion := flag.Bool("version", false, "Print version")
+	promServer := flag.String("promserver", "", "[REQUIRED] Prometheus Server to be used e.g. http://prometheus-server:9090")
 	logLevel := flag.String("log", defaultLogLevel, "Sets log level - INFO, DEBUG or TRACE")
 	port := flag.String("port", defaultPort, "The port to listen on for HTTP requests")
 	requestTimeout := flag.Int("timeout", defaultRequestTimeout, "HTTP request timeout in seconds for exporting Lustre Jobstats on Prometheus HTTP API")
-	urlLustreMetadataOperations := flag.String("urlMetadata", defaulturlLustreMetadataOperations, "Query URL to the Prometheus HTTP API that exports the Lustre metadata operations rate")
-	urlLustreJobReadBytes := flag.String("urlReads", defaultURLLustreJobReadBytes, "Query URL to the Prometheus HTTP API that exports the Lustre jobstats read throughput rate")
-	urlLustreJobWriteBytes := flag.String("urlWrites", defaultURLLustreJobWriteBytes, "Query URL to the Prometheus HTTP API that exports the Lustre jobstats write throughput rate")
+	timeRange := flag.String("timerange", defaultTimeRange, "Time range used for rate function on the retrieving Lustre metrics from Prometheus - A three digit number with unit s, m, h or d")
 
 	flag.Parse()
+
+	initLogging(*logLevel)
 
 	if *printVersion {
 		log.Info("Version: ", version)
 		os.Exit(0)
 	}
 
-	initLogging(*logLevel)
+	if *promServer == "" {
+		log.Panic("No Prometheus server has been specified")
+	}
 
 	metricsPath := "/metrics"
 	listenAddress := ":" + *port
 
 	log.Info("Exporter started")
 
-	e := newExporter(*requestTimeout, *urlLustreMetadataOperations, *urlLustreJobReadBytes, *urlLustreJobWriteBytes)
+	urlExports := newUrlExportLustreMetrics(*promServer, *timeRange)
+
+	e := newExporter(*requestTimeout, urlExports.metadataOperations, urlExports.jobReadBytes, urlExports.jobWriteBytes)
 	prometheus.MustRegister(e)
 
 	http.Handle(metricsPath, promhttp.Handler())
