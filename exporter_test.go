2e49c4d5064b80c33202453031df77f4e7a84fd9
Gabriele Iannetti <g.iannetti@gsi.de>
2024-01-02 10:54:45 +0100

    Update LICENCE

--- exporter_test.go
+++ exporter_test.go
@@ -1,3 +1,11 @@
+// -*- coding: utf-8 -*-
+//
+// © Copyright 2023 GSI Helmholtzzentrum für Schwerionenforschung
+//
+// This software is distributed under
+// the terms of the GNU General Public Licence version 3 (GPL Version 3),
+// copied verbatim in the file "LICENCE".
+
 package main
 
 import (
